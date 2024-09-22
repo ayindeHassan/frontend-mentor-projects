eec8df681b49378a5ef8f611333abd4186752187
Toheeb hassan <hassantoheebayinde@gmail.com>
2024-09-22 19:59:43 +0100

    Adding the dropdown functionality

--- banner-section/script.js
+++ banner-section/script.js
@@ -17,3 +17,27 @@ window.onload = updateImageSource();
 
 // Run the function whenever the window is resized
 window.onresize = updateImageSource();
+
+
+let dropDownBtn = document.querySelectorAll('.dropdown-container p');
+
+let dropDownContent = document.querySelectorAll('.dropdown-content');
+
+let dropdownImg = document.querySelectorAll('.dropdown-container img');
+console.log(dropdownImg)
+
+
+dropDownBtn.forEach((btn, index) => {
+    btn.addEventListener('click', ()=>{
+        dropDownContent[index].classList.toggle('dropdown-content');
+
+
+        if(dropDownContent[index].classList.contains('dropdown-content')){
+            dropdownImg[index].src = './images/icon-arrow-down.svg';
+        }
+        else{ 
+            dropdownImg[index].src = './images/icon-arrow-up.svg';
+        }
+    })
+
+});
